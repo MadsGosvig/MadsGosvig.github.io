df8fa5bea0c01f72f6fbdc1b4793e4d1f03912e1
Mads Gosvig <Gosvig.m@gmail.com>
2019-09-23 20:13:15 +0200

    Updates

---
+++ precache-manifest.eabd6012d3b8a1b32892eec9837f13da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c94d8f4167a1f28a6a5d477f2447cd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25501a1f45fd391d4d62",
+    "url": "/static/css/main.359e723d.chunk.css"
+  },
+  {
+    "revision": "23762a434c945e6be3de",
+    "url": "/static/js/2.b92e8d6e.chunk.js"
+  },
+  {
+    "revision": "25501a1f45fd391d4d62",
+    "url": "/static/js/main.7c90e501.chunk.js"
+  },
+  {
+    "revision": "81c2536a78df3513b9c3",
+    "url": "/static/js/runtime-main.6748f3a5.js"
+  },
+  {
+    "revision": "7f01d83d62d18af7565dbfa07d2845f9",
+    "url": "/static/media/logo.7f01d83d.svg"
+  }
+]);
